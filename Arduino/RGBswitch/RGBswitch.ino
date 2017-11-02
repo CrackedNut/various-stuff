8dcd79a9ba626debb09cb5be00f2162534343031
Luigi Pugliese <crackednut00@outlook.com>
2017-11-02 15:31:49 +0100

    Added arduino projects
    Added first arduino project for school

---
+++ Arduino/RGBswitch/RGBswitch.ino
@@ -0,0 +1,41 @@
+#define RED 6
+#define GREEN 5
+#define BLUE 4
+
+void red()
+{
+  digitalWrite(RED, HIGH);
+  digitalWrite(GREEN, LOW);
+  digitalWrite(BLUE, LOW);
+}
+
+void green()
+{
+  digitalWrite(RED, LOW);
+  digitalWrite(GREEN, HIGH);
+  digitalWrite(BLUE, LOW);
+}
+
+void blue()
+{
+  digitalWrite(RED, LOW);
+  digitalWrite(GREEN, LOW);
+  digitalWrite(BLUE, HIGH);
+}
+
+void setup()
+{
+  pinMode(RED, OUTPUT);
+  pinMode(GREEN, OUTPUT);
+  pinMode(BLUE, OUTPUT);
+}
+
+void loop()
+{
+  red();
+  delay(500);
+  green();
+  delay(500);
+  blue();
+  delay(500);
+}
