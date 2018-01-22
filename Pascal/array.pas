bb84561d62e239eb8183bcb4797d0653e10277e9
unknown <crackednut00@outlook.com>
2018-01-22 12:25:02 +0100

    modified array.pas

--- Pascal/array.pas
+++ Pascal/array.pas
@@ -8,31 +8,55 @@ type
 
 var
 	vec : v; 
+	scelta : Integer = 3;
+procedure presentazione();
+	begin
+		writeln('Questo è un programma che legge o stampa un array.')
+	end;
+
+procedure menu();
+	begin
+		writeln('1) leggi vettore');
+		writeln('2) stampa vettore') ;
+		writeln('0) esci');
+		write('Scelta: ');
+		readln(scelta);
+		writeln('');
+	end;
 
 procedure readv (var x : v);
-var
-	i : 1..dmax;
-begin
-	for i:= 1 to dmax do
+	var
+		i : 1..dmax;
 	begin
-		write('Elemento [',i, ']: ');
-		readln(x[i]);
-		writeln('')
+		for i:= 1 to dmax do
+		begin
+			write('Elemento [',i, ']: ');
+			readln(x[i]);
+			writeln('');
+		end;
 	end;
-end;
 
 procedure writev (var y : v);
-var
-	i : 1..dmax;
-begin
-	for i := 1 to 10 do
-		writeln('Elemento [', i, '] = ', y[i])
-end;
+	var
+		i : 1..dmax;
+	begin
+		for i := 1 to 10 do
+			writeln('Elemento [', i, '] = ', y[i])
+	end;
 
 begin
-	readv(vec);
-    writeln();
-	writev(vec);
-    write('Press any key to continue...');
+	presentazione();
+	while scelta <> 0 do 
+	begin
+		menu();
+		if scelta = 1 then 
+			readv(vec);  	
+			writeln();
+
+		if scelta = 2 then 
+			writev(vec);
+			writeln;
+	end;
+	writeln('Press any key to continue... ');
 	readln();
 end.
