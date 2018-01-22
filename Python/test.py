f5a99b4451e85131334bd9f857ff2039d2d6995d
Luigi Pugliese <crackednut00@outlook.com>
2018-01-22 21:11:53 +0100

    trying studio code

---
+++ Python/test.py
@@ -0,0 +1,22 @@
+
+"""
+importing shit
+
+"""
+import math
+
+""" 
+
+func
+
+"""
+def func(): #test
+    print("test\n")
+    aTry = input("num: ")
+    if aTry == 0:
+        print(math.pi)
+    else:
+        print(math.cos(math.pi))
+
+if __name__ == "__main__":
+    func()
