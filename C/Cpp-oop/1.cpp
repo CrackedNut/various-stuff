aea6a13fb1270ea85396dc138466c46d0e17a394
Luigi Pugliese <crackednut00@outlook.com>
2021-05-15 16:33:30 +0200

    yes

--- C/Cpp-oop/1.cpp
+++ C/Cpp-oop/1.cpp
@@ -1,43 +1,25 @@
 #include <iostream>
-#include <string>
-#include <sstream>
 
 using namespace std;
 
-class MyClass
-{
-  private:
-    int myNum;
-    string myString;
 
-  public:
-    void setNum(int n) {
-      myNum = n;
-    }
-    int getNum(){
-      return myNum;
-    }
-    void setStr(string s) {
-      myString = s;
-    }
-    string getString(){
-      return myString;
-    }
-    MyClass(string s, int n)
-    {
-      myNum = n;
-      myString = s;
-    }
-};
+int main() {
 
-int main(int argc, char const *argv[]) {
-  stringstream str;
-  str << argv[2]; int x; str >> x;
-  MyClass first("Hello", 1);
-  MyClass fromArg(argv[1], x);
-  cout << first.getString() << endl;
-  cout << first.getNum() << endl;
-  cout << fromArg.getString() << endl;
-  cout << fromArg.getNum() << endl;
+  int* b = new int[5];
+
+  for(int i= 0; i < 5; i++)
+    b[i] = i + 1;
+
+  cout << b << endl;
+  cout << &b << endl;
+
+  for(int i = 0; i < 5; i++)
+    cout << b[i] << endl;
+
+  for(int i = 0; i < 5; i++)
+    cout << &b[i] << endl;
+  delete [] b;
+
+
   return 0;
 }
